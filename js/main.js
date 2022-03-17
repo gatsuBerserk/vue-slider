b20a5fcd9c3a1f3f46ad8a30fb78a2a6103ded78
gatsuBerserk <berserkdev@outlook.it>
2022-03-17 20:13:26 +0100

    single selection

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ const app = new Vue(
     el : "#app",  
     data : { 
         title : "New Carousel",
-        // contatore 
-        activeElements : 0,  
+        activeElements : 0,
+        upScroll: null,  
         multimedia : [
             {
                 name : "Four elements",
@@ -57,71 +57,18 @@ const app = new Vue(
             }else{
                 this.activeElements--;
             }; 
-        },  
-
+        }, 
+        mouseChange : function(index){
+            this.activeElements = index;
+        }, 
     }
 } 
 )
-
-// let activeElement= 0; done
-// document.getElementsByClassName("my-img")[activeElement].classList.add("active");
-// document.getElementsByClassName("dom-preview")[activeElement].classList.add("filter"); 
-
-// const nextOne = document.querySelector("div.my-previous"); 
-
-// nextOne.addEventListener("click", function(){ 
-    
-//     document.getElementsByClassName("my-img")[activeElement].classList.remove("active"); 
-//     document.getElementsByClassName("dom-preview")[activeElement].classList.remove("filter"); 
-
-//     if(activeElement === 0){
-//         activeElement= multimedia.length -1; 
-//     }else{
-//         activeElement--;
-//     }; 
-
-//     document.getElementsByClassName("my-img")[activeElement].classList.add("active"); 
-//     document.getElementsByClassName("dom-preview")[activeElement].classList.add("filter");
-// }); 
-
-// const fewer = document.querySelector("div.my-next");  
-
-// fewer.addEventListener("click", function() {
-//     document.getElementsByClassName("my-img")[activeElement].classList.remove("active"); 
-//     document.getElementsByClassName("dom-preview")[activeElement].classList.remove("filter"); 
-   
-
-//     if( activeElement === multimedia.length -1){
-//         activeElement= 0;
-//     }else{
-//         activeElement++;
-//     }
-    
-//     document.getElementsByClassName("my-img")[activeElement].classList.add("active");
-//     document.getElementsByClassName("dom-preview")[activeElement].classList.add("filter");
-      
-// });   
 
 
 
 
 //  setInterval (next, 1500);
-
-
-// function next(){ 
-    
-//     document.getElementsByClassName("my-img")[activeElement].classList.remove("active");
-// 	document.getElementsByClassName("dom-preview")[activeElement].classList.remove("filter");
-	
-// 	if ( activeElement === multimedia.length - 1 ){
-// 		activeItem = 0;
-// 	} else {
-// 		activeElement++;
-// 	}
-// 	document.getElementsByClassName("my-img")[activeElement].classList.add("active");
-// 	document.getElementsByClassName("dom-preview")[activeElement].classList.add("filter");
-	
-// }; 
 
 
 
